51805b3caa80d0168cc3bf65899c62d82bb21384
Utkal97 <utkal.s15@iiits.in>
2020-05-25 00:53:51 +0530

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-A portfolio page created using Reactjs. Instead of building static website, this application is intended to add content on the fly.
---
+++ precache-manifest.9b0edd4db3d941e4c8a73a99f18c2671.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d411ee5f6bb75b72f077ccec094120e",
+    "url": "/profile-page-builder/index.html"
+  },
+  {
+    "revision": "d0dd5b1fe272b86c6420",
+    "url": "/profile-page-builder/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a86f79c8d2ebfbf74f56",
+    "url": "/profile-page-builder/static/css/main.92e14c0c.chunk.css"
+  },
+  {
+    "revision": "d0dd5b1fe272b86c6420",
+    "url": "/profile-page-builder/static/js/2.6c50c54b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/profile-page-builder/static/js/2.6c50c54b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a86f79c8d2ebfbf74f56",
+    "url": "/profile-page-builder/static/js/main.e2bb2b1a.chunk.js"
+  },
+  {
+    "revision": "9199df79235245890765",
+    "url": "/profile-page-builder/static/js/runtime-main.466cd248.js"
+  },
+  {
+    "revision": "5fcfed51220a56a82d706493b33dc35a",
+    "url": "/profile-page-builder/static/media/Resume.5fcfed51.pdf"
+  }
+]);

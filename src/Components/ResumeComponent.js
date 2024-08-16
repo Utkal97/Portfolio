928ec1dbaacd7ba776fe316e83e6992ade79a150
Utkal Sirikonda <usirikon@intralinks.com>
2024-08-16 15:14:47 -0400

    project setup

--- src/Components/ResumeComponent.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Resume({resume}) {
-    return (
-        <a href = {resume} target = "_blank" rel="noopener noreferrer">Get Resume</a>
-    );
-}
-
-export default Resume;
